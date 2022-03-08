0306080c5d2733e7a1c8fc43afdba6282956c1d0
[firstname <[tuannaph14620@fpt.edu.vn]>
2022-03-08 18:18:00 +0700

    y

--- dist/app.js
+++ dist/app.js
@@ -1,14 +1,15 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+console.log("okla ok");
 const a = 10;
 const b = 30;
 const myName = "Nguyen Anh Tuan";
 const myAge = 20;
 const status = true;
-const my0bj = {};
+const my0bj = { id: 1, name: "Tuan" };
 const numberArr = [1, 3, 3, 4];
 const stringArr = ["a", "b", "c"];
-const objectArr = [{ id: 1 }, { id: 2 }];
+const objectArr = [{ id: 1, name: "đd" }, { id: 2, name: "dđ" }];
 function sum(numA, numB) {
     return numA + numB;
 }
--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,22 @@
 export{}
+console.log("okla ok");
+
 const a: number = 10;
 const b: number = 30;
 
+type User = {
+    id: number;
+    name:string
+    status?: boolean
+}
 const myName: string ="Nguyen Anh Tuan";
 const myAge : number = 20;
 const status : boolean = true;
-const my0bj: object = {};
+const my0bj: User = {id:1, name: "Tuan"};
 
 const numberArr: number[] = [1,3,3,4];
 const stringArr: string[]= ["a","b","c"];
-const objectArr: object[] =[{id:1},{id:2}];
+const objectArr: User[] =[{id:1, name:"đd"},{id:2, name:"dđ"}];
 function sum(numA: number, numB: number):number{
     return numA + numB;
 }
