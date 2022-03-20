afc071c621d7c6bd9df74f3a81525c0534a994d3
[firstname <[tuannaph14620@fpt.edu.vn]>
2022-03-20 15:39:15 +0700

    add Product

---
+++ src/pages/ProductAdd.tsx
@@ -0,0 +1,36 @@
+import React from 'react'
+import { ProductType } from '../types/product'
+import {useForm, SubmitHandler} from 'react-hook-form'
+import { useNavigate } from 'react-router-dom'
+
+type ProductAddProps = {
+    onAdd : (product: ProductType) => void
+}
+
+type FormInput = {
+    name: string
+    price: number
+}
+
+const ProductAdd = (props: ProductAddProps) => {
+    const {register, handleSubmit, formState } = useForm<FormInput>()
+    const navigate = useNavigate()
+    const onSubmit: SubmitHandler<FormInput> = data =>{
+        props.onAdd(data)
+        navigate('/admin/products')
+    }
+  return (
+    <div className="max-w-7xl mx-auto py-6 sm:px-6 lg:px-8">
+  {/* Replace with your content */}
+  <form onSubmit={handleSubmit(onSubmit)}>
+    <input type="text" {...register('name', {required: true})} className="border border-black" placeholder="Name" /> <br />
+    <input type="number" {...register('price',{required:true})} className="border border-black" placeholder="Price" /> <br />
+    <button className="bg-blue-500 p-4 text-white">Thêm</button>
+  </form>
+  {/* /End replace */}
+</div>
+
+  )
+}
+
+export default ProductAdd
--- src/pages/productPage.tsx
+++ src/pages/productPage.tsx
@@ -1,39 +1,12 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
+import { ProductType } from "../types/product";
 
-const ProductPage = () => {
-  const product = [
-    {
-      id: 1,
-      name: "Tuấn",
-      img: "https://th.bing.com/th/id/OIP._Cmdbyfz0PzFk1KVCXR5gQHaE7?pid=ImgDet&rs=1"
-    },
-    {
-      id: 2,
-      name: "Đạt",
-      img:"https://img2.liberoquotidiano.it/upload/originals/1494320767173.jpg",
-      price: 10000
-      
-    },
-    {
-      id: 3,
-      name: "Cảy",
-      img: "https://th.bing.com/th/id/OIP.wOQFHyR-cf-VsYQp1G7LUAHaEK?pid=ImgDet&rs=1",
-      price: 10000
-    },
-    {
-      id: 4,
-      name: "Bảo",
-      img: "https://vnn-imgs-f.vgcloud.vn/2020/05/01/08/fred-mu.jpg",
-      price: 10000
-    },
-    {
-      id: 5,
-      name: "Thang",
-      img: "https://vnn-imgs-f.vgcloud.vn/2020/05/01/08/fred-mu.jpg",
-      price: 10000
-    },
-  ];
+type ProductPageProps = {
+  product: ProductType[]
+}
 
+const ProductPage = ({product}: ProductPageProps) => {
   return (
     <div>
       <h2 className="py-4 mt-10 font-bold text-blue-900 text-2xl uppercase">
@@ -42,9 +15,9 @@ const ProductPage = () => {
       <div className="news">
         <div className="flex flex-wrap justify-between  gap-px ">
           {product.map((item)=><div className="col w-1/6 p-4">
-            <a href="/#/news/${post.id}">
+            <NavLink to=''>
               <img className="w-full" src="https://th.bing.com/th/id/OIP._Cmdbyfz0PzFk1KVCXR5gQHaE7?pid=ImgDet&rs=1" />
-            </a>
+            </NavLink>
             <a href="/news/${post.id}">
               <h3 className="py-2 font-bold text-blue-900">
                 {item.name}
